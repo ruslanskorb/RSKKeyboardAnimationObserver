453adb86aa0b553691d4b0165bd0008dd625a683
Ruslan Skorb <ruslan.skorb@gmail.com>
2015-11-19 08:15:41 +0200

    [LICENSE+Demo+Sources] Update LICENSE.

--- UIViewController+RSKKeyboardAnimation/UIViewController+RSKKeyboardAnimation.h
+++ UIViewController+RSKKeyboardAnimation/UIViewController+RSKKeyboardAnimation.h
@@ -2,6 +2,8 @@
 // UIViewController+RSKKeyboardAnimation.h
 //
 // Copyright (c) 2015 Anton Gaenko
+// Copyright (c) 2015-present Ruslan Skorb, http://ruslanskorb.com/
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
@@ -19,6 +21,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
+//
 
 #import <UIKit/UIKit.h>
 
